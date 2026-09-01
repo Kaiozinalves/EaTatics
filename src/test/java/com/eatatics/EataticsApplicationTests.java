9f22cce5b99d01c426cca388623aa0a819fd4240
Kaio Victor Alves da Rocha <kaio@MacBook-Air-de-Kaio.local>
2026-09-01 17:08:08 -0300

    Alterações no application.properties

---
+++ src/test/java/com/eatatics/EataticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatatics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EataticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
